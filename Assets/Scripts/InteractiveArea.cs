d020492c3ca949a2e4847064cd727fe4955205c1
Zito3009 <riverzito09@gmail.com>
2026-05-18 14:01:42 -0300

    montiel al arcoo

---
+++ Assets/Scripts/InteractiveArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveArea : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Interactable interactableScript = other.GetComponent<Interactable>();
+        if (interactableScript != null)
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
